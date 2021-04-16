473101206f6bb4c7a9a3eee38b177827efc4ca97
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-04-16 13:01:16 -0700

    Completed task 6

--- index.js
+++ index.js
@@ -286,11 +286,10 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push({id: 20, name: 'Josh Scanlan', years: '1988 - present', genre: 'Web Design', nationality: 'American', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum lectus mauris ultrices eros.'})
+    return array;
   }
-
-  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
